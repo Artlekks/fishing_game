006dbc49cd66804ff019035275acd2af4246c7c5
Alex <artlekks@gmail.com>
2025-07-25 00:39:41 +0400

    Bar speed and filling up done, next the bar need to stay at its filling position when the player pressed K

--- scenes/power_bar_ui.gd
+++ scenes/power_bar_ui.gd
@@ -1,10 +1,10 @@
 extends Control
 
-@onready var bar = $Bar
+@onready var bar := $Bar
 
 var power := 0.0
 var direction := 1.0
-var speed := 1.5
+var speed := 0.75  # ← adjust this to slow it down
 var active := false
 
 func _ready():
@@ -14,16 +14,18 @@ func _ready():
 func _process(delta):
 	if active:
 		power += speed * delta * direction
+
 		if power >= 1.0:
 			power = 1.0
 			direction = -1.0
 		elif power <= 0.0:
 			power = 0.0
 			direction = 1.0
+
 		update_bar()
 
 func update_bar():
-	bar.scale.y = power  # or scale.x if horizontal
+	bar.scale.x = max(power, 0.001) if active else 0.0
 
 func reset():
 	power = 0.0
@@ -38,6 +40,6 @@ func start():
 
 func stop():
 	active = false
-	
+
 func get_power():
 	return power
