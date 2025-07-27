8a3c02f328da7a9997addc8439e4beed75ca6874
Alex <artlekks@gmail.com>
2025-07-27 18:15:07 +0400

    prep_fishing and fishing_idle working

--- scenes/player_3d.gd
+++ scenes/player_3d.gd
@@ -1,1 +1,17 @@
 extends Node3D
+
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
+@onready var anim_player = $AnimationPlayer
+
+func _ready():
+	anim_tree.active = true
+
+	print("▶ Starting prep_fishing")
+
+	anim_state.travel("prep_fishing")
+	await get_tree().create_timer(anim_player.get_animation("prep_fishing").length).timeout
+
+	print("▶ → fishing_idle")
+
+	anim_state.travel("fishing_idle")
