c1cdde01abb7fb6561528d9745c48b8d42441db8
Alex <artlekks@gmail.com>
2025-08-16 10:01:01 +0400

    Clean Wipe

--- scripts/fishing_test_scene.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-func _on_fish_zone_entered(zone):
-	print("Entered fish zone:", zone.zone_id)
-	# TODO: show "Press K to start fishing"
-
-func _on_fish_zone_exited(zone):
-	print("Exited fish zone:", zone.zone_id)
-	# TODO: maybe hide prompt or cancel prep
