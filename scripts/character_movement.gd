f907729181fb7880646e3c76e44f79c01315326e
Alex <artlekks@gmail.com>
2025-08-21 16:46:08 +0400

    CHaracterBody3D Movement

--- scripts/character_movement.gd
+++ scripts/character_movement.gd
@@ -1,12 +1,29 @@
 extends CharacterBody3D
 
-@export var speed = 1
+# How fast the player moves in meters per second.
+@export var speed = 14
 
-func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	velocity.x = input_direction.x * speed
-	velocity.z = input_direction.y * speed
+var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	get_input()
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Moving the Character
+	velocity = target_velocity
 	move_and_slide()
