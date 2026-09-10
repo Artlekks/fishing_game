dc8225d448c91d449e7b70b77ccec57c6ea17db3
Voljihya <13946802+Artlekks@users.noreply.github.com>
2026-09-10 11:45:50 +0400

    Cam rotation

--- scripts/camera_rig.gd
+++ scripts/camera_rig.gd
@@ -10,6 +10,21 @@ func _process(_delta: float) -> void:
 
 signal quarter_turned(direction: int)
 
+var is_rotating := false
+
 func rotate_quarter_turn(direction: int) -> void:
-	rotation.y += deg_to_rad(90.0 * direction)
+	if is_rotating:
+		return
+
+	is_rotating = true
 	quarter_turned.emit(direction)
+
+	var target_rotation := rotation.y + deg_to_rad(90.0 * direction)
+
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "rotation:y", target_rotation, 0.3)
+
+	await tween.finished
+	is_rotating = false
