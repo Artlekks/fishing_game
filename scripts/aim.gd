4f71b6eb482d36ff7c15de466aa08ba2bde61fcf
Voljihya <13946802+Artlekks@users.noreply.github.com>
2026-09-10 20:39:32 +0400

    Aim mechanic is done

--- scripts/aim.gd
+++ scripts/aim.gd
@@ -3,9 +3,12 @@ extends Node
 signal aim_changed(world_direction: Vector3)
 
 @export var turn_speed_degrees: float = 60.0
+@export var max_aim_angle_degrees: float = 60.0
 
 var active: bool = false
-var aim_yaw: float = 0.0
+
+var center_yaw: float = 0.0
+var aim_offset: float = 0.0
 
 
 func _process(delta: float) -> void:
@@ -17,7 +20,37 @@ func _process(delta: float) -> void:
 	if is_zero_approx(input):
 		return
 
-	aim_yaw += deg_to_rad(turn_speed_degrees) * input * delta
+	aim_offset += deg_to_rad(turn_speed_degrees) * input * delta
+
+	var max_angle := deg_to_rad(max_aim_angle_degrees)
+
+	aim_offset = clamp(
+		aim_offset,
+		-max_angle,
+		max_angle
+	)
+
+	_emit_aim()
+
+
+func start(initial_direction: Vector3) -> void:
+	center_yaw = atan2(
+		initial_direction.x,
+		initial_direction.z
+	)
+
+	aim_offset = 0.0
+	active = true
+
+	_emit_aim()
+
+
+func stop() -> void:
+	active = false
+
+
+func _emit_aim() -> void:
+	var aim_yaw := center_yaw + aim_offset
 
 	var direction := Vector3(
 		sin(aim_yaw),
@@ -26,14 +59,3 @@ func _process(delta: float) -> void:
 	)
 
 	aim_changed.emit(direction)
-
-
-func start(initial_direction: Vector3) -> void:
-	aim_yaw = atan2(initial_direction.x, initial_direction.z)
-	active = true
-
-	aim_changed.emit(initial_direction.normalized())
-
-
-func stop() -> void:
-	active = false
